7b9c666dd193959c88ec2a833573db104f9d5609
SabadoEli <esabado@ualr.edu>
2024-09-11 15:46:48 +0000

    Update 09/11/2024

--- 03.Project.py
+++ 03.Project.py
@@ -2,5 +2,11 @@ x = float(input("Enter First Number: "))
 y = (input("Enter Operator(+,-,*,/): "))
 z = float(input("Enter Second Number: "))
 
-if y == '+'
+if y == '+':
     (print)(x + z)
+if y == '-':
+    (print)(x-z)
+if y == '*':
+    (print)(x*z)
+if y == '/':
+    (print)(x/z)
