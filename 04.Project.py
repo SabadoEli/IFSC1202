ce19de8205c5e7f8ad0cf9945a232991461a41c5
SabadoEli <esabado@ualr.edu>
2024-09-18 22:11:46 +0000

    Update 5:11 PM

--- 04.Project.py
+++ 04.Project.py
@@ -1,3 +1,10 @@
 start = int(input("Enter Start of Range: "))
 end = int(input("Enter End of Range: "))
-print("Prime Numbers between {start} and {end}:")
+print(f"Prime Numbers between {start} and {end}:")
+
+full_list=[]
+prime_list = []
+
+for num in range(start, end +1):
+    full_list.append(num)
+    if 
