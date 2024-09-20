41d16d6b5961c003990eba6b6eef0386b035cdf4
SabadoEli <esabado@ualr.edu>
2024-09-20 06:48:34 +0000

    Update Sep 20

--- 04.Project.py
+++ 04.Project.py
@@ -1,10 +1,22 @@
-start = int(input("Enter Start of Range: "))
-end = int(input("Enter End of Range: "))
-print(f"Prime Numbers between {start} and {end}:")
+first_number = int(input("Enter Start of Range: "))
+second_number = int(input("Enter End of Range: "))
 
-full_list=[]
-prime_list = []
+start = min(first_number, second_number)
+end = max(first_number, second_number)
+
+primes = []
 
 for num in range(start, end +1):
-    full_list.append(num)
-    if 
+    if num < 2: 
+        continue
+    is_prime = True
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            is_prime = False
+            break 
+        
+    if is_prime:
+            if num not in primes:
+                primes.append(num)
+    
+print(f"Prime Numbers between {first_number} and {second_number}: {primes}")
